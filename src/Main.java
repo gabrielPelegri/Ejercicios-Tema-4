14e60d03990fab4e4b4ee70e487e08b9a831ea37
vnavacar <vnavacar@myuax.com>
2022-02-28 00:29:01 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,43 +7,22 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    	Random rand = new Random();
-    	int int_random = rand.nextInt(101); // el numero aleatorio
+    	int numero = 1;
+    	
     	
         Scanner teclado = new Scanner(System.in);
-        System.out.println("Elija un número del 1 al 100, luego presione cualquier tecla.");
-        char tecla = teclado.next().charAt(0);
-        int minimo = 0;
-        int maximo = 100;
-        
-        while(tecla != '=') {
-        	
-        	int_random = rand.nextInt(minimo, maximo);
-        	System.out.println("Pruebo con" +int_random+", ¿es mayor, menor o es el número (+/-/=)?");
-        	
-            while (true) {
-                try {
-                    tecla = teclado.next().charAt(0);
-                    break;
-                } catch (InputMismatchException e) {
-                	System.out.println("Vuelva a intentar:");
-                    teclado.next();
-                }
-            }
-        	
-        	
-        	if(tecla == '+') {
-        		minimo = int_random +1;
+        System.out.println("Escriba un multiplo de 3:");
+        //while(numero%3==0) {
+        do {
+        	numero= teclado.nextInt();
+        	if(numero%3!=0) {
+        		System.out.println("Error "+numero+" no es un multiplo de 3");
         	}
-        	else if(tecla == '-') {
-        		maximo = int_random -1;
-        	}
-        	else if(tecla != '='){
-        		System.out.println("Vuelva a intentar:");
-        	}
-        }
+        	
+        }while(numero%3==0)
+        	
         teclado.close();
-        System.out.println("Bravo, lo he encontrado!");
+        System.out.println("Ok:"+numero+"es un multiplo de 3");
         
 
 
